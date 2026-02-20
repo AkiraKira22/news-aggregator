9f37a92d196a57e9dd1d72caeb85f48028623915
Hellocat22 <riyakavajravesaka@gmail.com>
2026-02-20 17:17:54 +0800

    Added TopHeadlines to show headline list

--- client/src/components/TopHeadine.jsx
+++ client/src/components/TopHeadine.jsx
@@ -3,7 +3,8 @@ import Card from "./Card";
 import Loader from "./Loader";
 import { use } from "react";
 
-function NewsPage() {
+function TopHeadline() {
+    const params = useParams();
     const [ data, setData ] = useState([]);
     const [ page, setPage ] = useState(1);
     const [ totalResults, setTotalResults ] = useState(0);
@@ -20,24 +21,31 @@ function NewsPage() {
     let pageSize = 6;
 
     useEffect(() => {
-        fetch(`http://localhost:3000/all-news?page=${page}&pageSize=${pageSize}`)
+        const categoryParam = params.category ? `&category=${params.category}` : '';
+        fetch(`http://localhost:3000/top-headlines?&language=en${categoryParam}&page=${page}&pageSize=${pageSize}&apiKey=${API_KEY}`)
         .then(response  => {
-            if (response.ok) {
-                setIsLoading(true);
-                return response.json();
+            if (!response.ok) {
+                throw new Error('Failed to fetch data');
+                // setIsLoading(true);
+                // return response.json();
             }
+            return response.clone().json();
         })
-        .then (myJson => {
+        .then (json => {
             setTotalResults(myJson.data.totalResults);
             setData(myJson.data.articles);
-        })
         setIsLoading(false);
-    }, [page])
+        })
+        .catch (error => {
+            console.error('Error fetching data:', error);
+            setIsLoading(false);
+    });
+    }, [page, params.category])
 
     return (
         <>
             <div className='my-10 cards grid lg:place-content-center md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 xs:grid-cols-1 xs:gap-4 md:gap-10 lg:gap-14 md:px-16 xs:p-3 '>
-                {isLoading ? data.map((element, index) => (
+                {isLoading ? (data.length > 0 ? ( data.map((element, index) => (
                     <Card 
                         title={element.title}
                         description={element.description}
@@ -49,7 +57,14 @@ function NewsPage() {
                         key={index}
                     />
                     
-                )) : <Loader />}
+                ))
+                ) :( 
+                    <p>No articles available for this category.</p>
+                )
+                ) :(
+                    <Loader />
+                )}
+
             </div>
             {isLoading && <div className="pagination flex justify-center gap-14 my-10 items-center">
                 <button disabled={page <= 1} className="pagination-btn" onClick={() => handlePrevPage()}>Previous</button>
@@ -61,4 +76,4 @@ function NewsPage() {
     )
 }
 
-export default NewsPage;
+export default TopHeadline;
